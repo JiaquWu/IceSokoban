df8d8627f63ddd22728765c63244d1d1dc603920
Rabbit-Adobe <rabbitadobewang@gmail.com>
2022-12-11 17:52:03 +0100

    Wwise integration and footstep event
    integrate Wwise into project
    add event in walking animation linking to footstep

---
+++ Assets/Scripts/Audio/Walking.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public AK.Wwise.Event PlayFoostep;    
+
+    public void PlayFootstep()
+    {
+        Debug.Log("Play footstep sound");
+    }
+}
