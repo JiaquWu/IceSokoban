5b19ae7b70357be5327db848ad8c5cd6d215de97
Rabbit-Adobe <rabbitadobewang@gmail.com>
2022-12-12 03:09:54 +0100

    submit pushing sound script
    so far nothing affect actual game play
    still need implement audio assets and Wwise resources

---
+++ Assets/Scripts/Audio/Pushing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class Pushing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AK.Wwise.Event PlayPushing;
+
+    //需要添加判断
+    //土地上推动播放Pushing_short，冰面上推动播放Pushing_long
+
+    //还需考虑 先在冰面上推动，遇到土地停下的情况
+
+    public void PlayPushingSound()
+    {
+        Debug.Log("Play pushing sound");
+    }
+}
